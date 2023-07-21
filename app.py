528e23524983a760226023d89e8287629e799b09
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-07-21 04:26:26 +0100

    done

--- app.py
+++ app.py
@@ -1,5 +1,1 @@
 from app import create_app
-import os
-app = create_app
-
-# app.run(debug=False, port=os.getenv("PORT") or 8000)
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from dotenv import dotenv_values
+from .utils.helpers import response
 
 ENV = dotenv_values()
 
@@ -13,5 +14,14 @@ def create_app():
   # ROUTES
   from .routes.notes import notes
   app.register_blueprint(notes, url_prefix="/api/v1/notes")
+
+  # ERROR ROUTES
+  app.errorhandler(404)
+  def invalid_route():
+    return response("Invalid route", None, False)
+  
+  app.errorhandler(500)
+  def server_error():
+    return response("Something went wrong, please try again", None, False)
 
   return app
